9d7c0a7621053cbe5295d43da53991b223a99188
nedeljkokostic96 <nedeljkokostic96@gmail.com>
2020-01-21 16:21:08 +0100

    WEB

---
+++ Planinarsko_drustvo_WEB/src/main/java/com/example/demo/PlaninarskoDrustvoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaninarskoDrustvoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaninarskoDrustvoWebApplication.class, args);
+	}
+
+}
